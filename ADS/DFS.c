9604aa49a85de30c3ab5ba831f77743aaf2f560a
tvmcoder <anandupn@outlook.com>
2024-12-23 21:39:48 +0530

    DFS completed

--- ADS/DFS.c
+++ ADS/DFS.c
@@ -1,18 +1,17 @@
 #include<stdio.h>
-void dfs(int n,int g[n][n],int v[],int source);
 void dfs(int n,int g[n][n],int v[],int source){
-    printf("%d-> ",source);
+    printf("%d -> ",source);
     v[source]=1;
     for(int i=0;i<n;i++){
         //recursive function called when adajcent node is not already visited
-        if(v[source]!=1 && g[source][i]!=0){
-            gfs(n,g,v,i);
+        if(v[i]!=1 && g[source][i]!=0){
+            dfs(n,g,v,i);
         }
     }
 }
  int main(){
     int n,i,j,source;
-    printf("\nenter the size if node:");
+    printf("\nEnter the size if node:");
     scanf("%d",&n);
     int v[n];
     for(i=0;i<n;i++){
@@ -21,14 +20,15 @@ void dfs(int n,int g[n][n],int v[],int source){
     //adjacency matrix
     int g[n][n];
     for(i=0;i<n;i++){
+        printf("\nNext row:");
         for(j=0;j<n;j++){
-            printf("\nEnter the edges");
-            scanf("%d",g[i][j]);
+            printf("\nEnter the edges:");
+            scanf("%d",&g[i][j]);
         }
     }
-    printf("\nEnter the source");
-    scanf("%d",source);
-    if(source < n || source >n){
+    printf("\nEnter the source:");
+    scanf("%d",&source);
+    if(source < 0 || source >n){
         printf("invalid source");
         return 0;
     }
