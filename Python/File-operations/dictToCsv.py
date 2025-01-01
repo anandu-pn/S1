5c746276898658f7a5218d1a9d97a9317061c23e
tvmcoder <anandupn@outlook.com>
2025-01-01 16:35:34 +0530

    fileOperation&Csv

---
+++ Python/File-operations/dictToCsv.py
@@ -0,0 +1,21 @@
+import csv
+dict={ 'Name': ['Alice', 'Bob', 'Charlie'], 
+ 'Age': [25, 30, 35], 
+ 'City': ['New York', 'Los Angeles', 'Chicago']
+}
+columns=dict.keys()
+file=open('C:/Users/user/Desktop/lab/S1/Python/File-operations/newemployee.csv','w',newline='')#this newline is important
+writer=csv.writer(file)
+writer.writerow(columns)#write first column 
+#data.values() returns a view object that displays a list of all the values in the dictionary.
+#The * operator is used to unpack the values, so each list of values is passed as a separate argument to zip.
+#zip then combines these lists element-wise, creating tuples where each tuple contains one element from each list.
+rows=zip(*dict.values())
+for row in rows:
+    writer.writerow(row)
+file.close()
+file=open('C:/Users/user/Desktop/lab/S1/Python/File-operations/newemployee.csv','r')
+rows1=csv.reader(file)
+for row in rows1:
+    print(row)
+file.close()
--- Python/File-operations/readCsvColumn.py
+++ Python/File-operations/readCsvColumn.py
@@ -1,3 +1,16 @@
-import pandas as pd
+#here implement using pandas odule
+"""import pandas as pd
 df=pd.read_csv('C:/Users/cacet/Desktop/anan/S1/Python/File-operations/employee.csv')
-print(df.columns)
+#read the first column and all the row in first column
+column=df.iloc[:,0]
+print(column)"""
+#implemented using csv module
+import csv
+file=open('C:/Users/user/Desktop/lab/S1/Python/File-operations/employee.csv','r')
+reader=csv.reader(file)
+#it is possible to use to metod column name and column index index to print the values in column use any as required
+#column_index=0#first colum instead
+header=next(reader) #using column name to find column
+column_index=header.index('Name')
+for row in reader:
+    print(row[column_index])
