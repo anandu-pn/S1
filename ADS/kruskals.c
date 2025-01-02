edfdf5beef6d2621c3693d924ec5afad08bcaf21
tvmcoder <anandupn@outlook.com>
2025-01-02 17:15:39 +0530

    Kruskals

--- ADS/kruskals.c
+++ ADS/kruskals.c
@@ -1,91 +1,74 @@
 #include<stdio.h>
 #include<stdlib.h>
-int n,e;
+int n,e,parent[100];
+typedef struct{
+    int u,v,w;
+}edge;
+//find and union are operation in disjoint set data structure
+int find(int i){
+    while(parent[i]!=i){
+        i=parent[i];
+    }
+    return i;
+}
+void Union (int U,int V){
+    int p1,p2;
+    p1=find(U);
+    p2=find(V);
+    parent[p1]=p2;
+}
+
+edge Edges[100];
+void kruskals(){
+    int j,i,U,V;
+    int mincost=0;
+    edge temp;
+    for(i=0;i<n;i++){
+        parent[i]=i;
+    }
+//to order by weight
+    for (i = 0; i < e - 1; i++) {
+        for (j = 0; j < e -1; j++) {
+            if (Edges[j].w > Edges[j + 1].w || (Edges[j].w == Edges[j + 1].w && Edges[j].u > Edges[j + 1].u)) {
+                temp = Edges[j];
+                Edges[j] = Edges[j + 1];
+                Edges[j + 1] = temp;
+            }
+        }
+    }
+    for(i=0;i<e;i++){
+        printf("{%d - %d = %d}\n",Edges[i].u,Edges[i].v,Edges[i].w);
+    }
+    printf("\nminimum spanning tree:\n");
+//we need to check weher there are any closed edges
+    for(i=0;i<e;i++){
+        U=find(Edges[i].u);
+        V=find(Edges[i].v);
+        if(U!=V){
+            printf("{%d - %d = %d}\n",Edges[i].u,Edges[i].v,Edges[i].w);
+            mincost+=Edges[i].w;
+            Union(Edges[i].u,Edges[i].v);
+        }
+        printf("Parent array after processing edge {%d - %d = %d}: ", Edges[i].u, Edges[i].v, Edges[i].w); 
+        for (j = 0; j < n; j++) { 
+            printf("%d ", parent[j]); 
+            } 
+            printf("\n"); 
+    }
+    printf("Minimum cost = %d\n", mincost);
+
+}
+
 int main(){
     int i;
     printf("Enter number of vertices:");
     scanf("%d",&n);
     printf("\nEnter number of edges:");
     scanf("%d",&e);
-    for(i=0;i<e;e++){
-        printf("\nEnter the edge %d",i);
-        scanf("");
+    for(i=0;i<e;i++){
+        printf("\nEnter the edge u,v,w :");
+        scanf("%d %d %d",&Edges[i].u,&Edges[i].v,&Edges[i].w);
     }
-
+    kruskals();
 return 0;
 }
-/*
-#include <stdio.h>
-#include <stdlib.h>
-
-#define MAX 100
-
-typedef struct {
-    int u, v, w;
-} Edge;
-
-Edge edges[MAX];
-int parent[MAX];
-int n, e;
-
-int find(int i) {
-    while (parent[i] != i)
-        i = parent[i];
-    return i;
-}
-
-void union1(int i, int j) {
-    int a = find(i);
-    int b = find(j);
-    parent[a] = b;
-}
-
-void kruskal() {
-    int i, j, u, v;
-    Edge temp;
-    int mincost = 0;
-
-    for (i = 0; i < n; i++)
-        parent[i] = i;
-
-    for (i = 0; i < e - 1; i++) {
-        for (j = 0; j < e - i - 1; j++) {
-            if (edges[j].w > edges[j + 1].w) {
-                temp = edges[j];
-                edges[j] = edges[j + 1];
-                edges[j + 1] = temp;
-            }
-        }
-    }
-
-    printf("Edges in the minimum spanning tree:\n");
-    for (i = 0; i < e; i++) {
-        u = find(edges[i].u);
-        v = find(edges[i].v);
-        if (u != v) {
-            printf("%d - %d: %d\n", edges[i].u, edges[i].v, edges[i].w);
-            mincost += edges[i].w;
-            union1(u, v);
-        }
-    }
-    printf("Minimum cost = %d\n", mincost);
-}
-
-int main() {
-    int i;
-    printf("Enter the number of vertices: ");
-    scanf("%d", &n);
-    printf("Enter the number of edges: ");
-    scanf("%d", &e);
-
-    printf("Enter the edges (u v w):\n");
-    for (i = 0; i < e; i++) {
-        scanf("%d %d %d", &edges[i].u, &edges[i].v, &edges[i].w);
-    }
-
-    kruskal();
-
-    return 0;
-}
-
-*/
